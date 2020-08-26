5a551f8eff5efae0fc8a73a0f4d26dbf2f2a8e38
whwndgus001 <whwndgus0000@naver.com>
2020-08-26 16:07:32 +0900

    들여쓰기

---
+++ 01/helloworld.py
@@ -0,0 +1,31 @@
+# helloworld.py
+
+#블록 시작(전역)
+print ('hello world')
+a = 2
+if a > 1: #블록 시작(조건문)
+    b = 1
+    print(a)
+    print('big')
+    # 블록 끝(조건문)
+
+
+def my_func(i):
+    print('블록 시작(함수정의)')
+
+    if i > 10:
+        print('새블록 시작(조건문)')
+
+    print('big')
+
+    print('...')
+
+    print('블록 끝(함수정의)')
+
+#함수 호출
+my_func(20)
+
+
+
+
+
