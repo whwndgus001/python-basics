79d4ed4ef95eb245e4dbe234f403a3f28b620ee4
whwndgus001 <whwndgus0000@naver.com>
2020-08-27 15:58:04 +0900

    자료형 연습#3

---
+++ 04/01.condition.py
@@ -0,0 +1,29 @@
+# if ~ else
+
+a = 2
+if a > 5:
+    print('big')
+else:
+    print('small')
+
+# if ~ elif ~ elif ~ else
+n = 1000
+if n > 0:
+    print('양수')
+elif n < 0:
+    print ('음소')
+else :
+    print ('0')
+
+if order == 'spam':
+    price = 1000
+elif order == 'egg':
+    price = 200
+elif order == 'spagetti':
+    price = 2000
+
+print('order: %s, price%d' % (order, price))
+
+
+
+
---
+++ 04/02.fow.py
@@ -0,0 +1,33 @@
+# for 반복문
+# for   in  [sequnce 객체] :
+for number in [10, 20, 30, 40] :
+    result = number**2
+    print (result, '----', end=' ')
+else:
+    print('')
+
+
+a = ['cat', 'cow', 'tiger']
+for animal in a:
+    print(animal, end=' ')
+else:
+    print('')
+
+# 복합 자료형을 for문에서 사용하는 경우
+l = [('둘리', 10), ('마이콜', 30),('또치', 11)]
+for t in l:
+    print('이름: %s, 나이:%d' %t)
+
+# 10번 반복하는 Loop
+for i in range(1, 11):
+    print(i, end=' ')
+else:
+    print('\n---------------------')
+
+# 1 ~ 10 합을 구하기
+sum = 0
+
+
+for i in range(1, 11):
+    sum=sum+i
+print(sum)
