842611cb8e9b715d6c0a3e77e77530f8c384ac51
whwndgus001 <whwndgus0000@naver.com>
2020-08-28 16:52:52 +0900

    연습문제 추가

---
+++ produce02/02.prodc2.py
@@ -0,0 +1,17 @@
+문제2. 다음과 같은 텍스트에서 모든 태그를 제외한 텍스트만 출력하세요.
+실행 결과:
+‘usr’, ‘local’, ‘bin’, ‘python’
+
+실행 결과:
+‘/usr/local/‘bin’, ‘python’
+
+s = &quot;&quot;&quot;
+&lt;html&gt;
+&lt;body style=&#39;background-color:#ffff&#39;&gt;
+&lt;h4&gt;Click&lt;/h4&gt;
+&lt;a href=&#39;http://www.python.org&#39;&gt;Here&lt;/a&gt;
+&lt;p&gt;
+To connect to the most powerful tools in the world.
+&lt;/p&gt;
+&lt;/body&gt;
+&lt;/html&gt;&quot;&quot;&quot;
---
+++ produce02/03.prodc3.py
@@ -0,0 +1,7 @@
+# 문제 3.
+#
+# 1) 다음 문자열은 모든 소문자를 대문자로 변환하고, 문자 ',', ',' '\n'를 없앤 후에 중복 없이 각 단어를 순서대로 출력하세요.
+# s ="""We encourage everyone to contribute to Python.
+# If you still have questions after reviewing the material
+# in this guide,
+# then the Python Mentors group is available to help guide new contributors through the process."""
