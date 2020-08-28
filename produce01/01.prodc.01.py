4d4eba25e0a5d43e002b34cbb1fb3acabefde835
whwndgus001 <whwndgus0000@naver.com>
2020-08-28 11:53:07 +0900

    list 연습

--- produce01/01.prodc.01.py
+++ produce01/01.prodc.01.py
@@ -1,12 +1,15 @@
 # 문제 1. 키보드로 정수 수치를 입력 받아 그것이 3의 배수인지 판단하세요.
-a = int(input ('수를 입력하세요:'))
 
-if a%3 ==0:
-    print('3의 배수')
-elif a%3 !=0:
-    print('3의 배수가 아닌 수')
-else :
-    #print
+try:
+    a = int(input('수를 입력하세요:'))
+    if a%3 ==0:
+        print('3의 배수')
+    elif a%3 !=0:
+        print('3의 배수가 아닌 수')
+    else:
+except ValueError as e:
+    print('정수가 아닙니다')
+
 # import sys
 #
 # number = input ('수를 입력하세요:')
