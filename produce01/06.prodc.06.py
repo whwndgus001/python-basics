1fb68b78b47a4dbed82883209a1f02cdddb9b47d
whwndgus001 <whwndgus0000@naver.com>
2020-09-01 09:51:48 +0900

    Symbol Table 추가

--- produce01/06.prodc.06.py
+++ produce01/06.prodc.06.py
@@ -4,8 +4,27 @@
 # 되는지 작성하시오.
 # comprehension
 
+a = int(input('정수를 입력하세요 : '))
 moneys = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
-money = 123456
+money = []
+
 
 count_50000 = money // 50000
-money -= count_50000*50000
+count_10000 = money // 10000
+count_5000 = money // 5000
+count_1000 = money // 1000
+count_500 = money // 500
+count_100 = money // 100
+count_50 = money // 50
+count_10 = money // 10
+count_1 = money // 1
+
+print("50000권의 개수", count_50000)
+print("10000권의 개수", count_10000)
+print("5000원권의 개수", count_5000)
+print("1000원권의 개수", count_1000)
+print("500원권의 개수", count_500)
+print("100원권의 개수", count_100)
+print("50원권의 개수", count_50)
+print("10원권의 개수", count_10)
+print("1원권의 개수", count_1)
