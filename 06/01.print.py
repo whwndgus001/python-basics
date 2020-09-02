669004c8df2cc0632bc94d4c95eda83296d8cc4a
whwndgus001 <whwndgus0000@naver.com>
2020-09-02 13:50:41 +0900

    io 연습 #1

---
+++ 06/01.print.py
@@ -0,0 +1,35 @@
+import sys
+# 가변인수
+
+print(1)
+print('hello', ' ', 'world')
+
+# sep 기본 인수값
+x = 0.2
+s = 'hello'
+print(str(x) + ' ' +  s)
+print(x, s)
+print(x, s, sep=' ')
+print(x, s, sep=':')
+
+# end 기본 인수값
+print('hello world')
+print("!!!!!!!!!!!!!!!!!")
+print('hello world', end='\n')
+print("!!!!!!!!!!!!!!!!!")
+print('hello world', end='')
+print("!!!!!!!!!!!!!!!!!")
+
+print('=== file 기본 인수값 ===')
+print('hello world')
+print('hello world', file=sys.stdout)
+print('hello world', file=sys.stderr)
+
+f = open('hello.txt', 'w')
+print(type(f))
+print('hello world', file=f)
+f.close()
+
+# 참고
+sys.stdout.write('hello world!!!!!!!!!!!!!')
+
